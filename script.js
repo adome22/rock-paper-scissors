2cb1cf8e60c845be7af52f666c3d504398ed0ffa
adome <62933497+adome22@users.noreply.github.com>
2024-01-25 19:20:34 +0200

    Added a for-loop and comments

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-function getComputerChoice() {
-    const choice = ["rock", "paper", "scissors"];
-    return choice[Math.floor(Math.random() * choice.length)];
-}
+// Loop for 5 games.
+for (let count = 0; count < 5; count++) {
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a draw. Go again!";
+    // Randomly chooses the computer choice for rock, paper and scissors.
+    function getComputerChoice() {
+        const choice = ["rock", "paper", "scissors"];
+        return choice[Math.floor(Math.random() * choice.length)];
     }
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You are a winner! Rock beats scissors.";
-    }
-    if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You are a winner! Paper beats rock.";
-    }
-    if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You are a winner! Scissors beat paper.";
-    } else {
-        return `You lost :( ${computerSelection} beats ${playerSelection}`;
-    }
-}
 
-const playerSelection = prompt("Rock, Paper or Scissors?");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+    // Get user input.
+    const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+    // ComputerChoice to constant.
+    const computerSelection = getComputerChoice();
+
+    // Logic of the game.
+    function game(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "It's a draw. Go again!";
+        }
+        if (playerSelection === "rock" && computerSelection === "scissors") {
+            return "You are a winner! Rock beats scissors.";
+        }
+        if (playerSelection === "paper" && computerSelection === "rock") {
+            return "You are a winner! Paper beats rock.";
+        }
+        if (playerSelection === "scissors" && computerSelection === "paper") {
+            return "You are a winner! Scissors beat paper.";
+        } else {
+            return `You lost :( ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+    // Making a variable for the results.
+    let result = game(playerSelection, computerSelection);
+
+    // Result of each round.
+    console.log(result);
+}
