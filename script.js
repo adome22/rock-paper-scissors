cbba478b66d759dadcb1faac69d8fec9186a5b5b
adome <62933497+adome22@users.noreply.github.com>
2024-01-25 13:50:25 +0200

    Added JavaScript for CPU choice.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function getComputerChoice() {
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+console.log(getComputerChoice());
