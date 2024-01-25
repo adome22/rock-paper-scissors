7d497a4f76e6947034d8ee40130dc135c70418e4
adome <62933497+adome22@users.noreply.github.com>
2024-01-25 17:05:45 +0200

    Added playRound function to determine winner.

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-
-
 function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
+    const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw. Go again!";
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You are a winner! Rock beats scissors.";
+    }
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You are a winner! Paper beats rock.";
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You are a winner! Scissors beat paper.";
+    } else {
+        return `You lost :( ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = prompt("Rock, Paper or Scissors?");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
